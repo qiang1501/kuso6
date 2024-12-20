ad338996769aa025c6df2d16d1e9b94eb9e9e1f0
qiang1501 <hongqiang365@gmail.com>
2024-12-20 22:40:20 +0900

    Add .nojekyll file and update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  // base: '/firework/',
-  base: '/kuso/',
+  base: '/kuso2/', // 设置为 GitHub Pages 的子路径
   plugins: [react()],
 })
